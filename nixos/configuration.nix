fc3149c3600b376d69c2c90d569c36045f180ada
umpug <141457520+kUmutUK@users.noreply.github.com>
2026-04-30 23:12:11 +0300

    Add files via upload

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -308,7 +308,7 @@
     kitty waybar rofi dunst awww waypaper grim slurp wl-clipboard
     hyprlock hypridle wlogout hyprpicker
     networkmanagerapplet brightnessctl playerctl
-    pavucontrol cliphist
+    pavucontrol cliphist gcc pkg-config openssl webkitgtk_4_1 libappindicator-gtk3
     kdePackages.dolphin stdenv.cc.cc.lib
     polkit_gnome ntfs3g exfat gparted
     steam gamemode gamescope mangohud
